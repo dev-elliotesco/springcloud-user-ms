2947975e8a086677d95ddcd69970c4fd47186b49
Elliot Escovitch <dev.escovitch.ri@gmail.com>
2024-08-13 00:00:46 -0500

    add custom log for the app init

--- src/main/java/com/user/ms/UserMsApplication.java
+++ src/main/java/com/user/ms/UserMsApplication.java
@@ -1,13 +1,19 @@
 package com.user.ms;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserMsApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(UserMsApplication.class);
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(UserMsApplication.class, args);
+		logger.info("Welcome to User Microservice!");
 	}
 
 }
